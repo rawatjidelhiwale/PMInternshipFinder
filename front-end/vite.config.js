7aeab34cb50367fd80783b10d8d62246d0902877
Pushker Rawat <pushkarrawat694@gmail.com>
2025-09-11 22:32:46 +0530

    Update vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,10 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [
-    react(),tailwindcss(),base: '/PMInternshipFinder/'
+    react(),
+    tailwindcss()
   ],
+  // <-- put base at top level
+  base: '/PMInternshipFinder/',
 })
